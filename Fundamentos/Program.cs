3b0dffe72657383e113888d650fe367146b474ef
Leilane <leilane240512@gmail.com>
2023-04-30 17:12:42 -0300

    Atualizações

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,30 @@
+﻿using Fundamentos.ApresentarDados;
+using Fundamentos.EntradaDados;
+using Fundamentos.EstruturaCondicional;
+using Fundamentos.LacoRepeticao;
+using Fundamentos.VetorComFor;
+using Fundamentos.Vetores;
+
+//ApresentarDados01 executor = new ApresentarDados01();
+//executor.Executar();
+
+
+//EstruturaCondicionalAnd executor = new EstruturaCondicionalAnd();
+//executor.Executar();
+
+//EntradaDados01 executor2 = new EntradaDados01();
+//executor2.Executar();
+
+//executor.Executar();
+
+//EstruturaCondicionalOu executor = new EstruturaCondicionalOu();
+//executor.Executar();
+
+//EstruturaCondicionalAvancada executor = new EstruturaCondicionalAvancada();
+//executor.Executar();
+
+//LacoRepeticaoWhile executor = new LacoRepeticaoWhile();
+//executor.Executar();
+
+ExemploVetorComFor executor = new ExemploVetorComFor();
+executor.Executar();
